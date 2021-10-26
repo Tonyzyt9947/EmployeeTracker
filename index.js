507630688e2620ace56779176dfcf97457783aea
TZ <tz116917@gmail.com>
2021-10-25 20:36:18 -0700

    inquirer questions

--- index.js
+++ index.js
@@ -20,7 +20,66 @@ db.query('SELECT * FROM roles', function (err, results) {
     console.table(results);
 });
 
-db.query("SELECT * FROM employees WHERE role_id = '2' ", function (err, results) {
+db.query("SELECT * FROM employees JOIN roles ON employees.role_id = roles.id", function (err, results) {
     console.log(err)
     console.table(results);
 });
+
+
+const menu = [
+    {
+        type: 'list',
+        name: 'menu',
+        message: 'Select an action:',
+        choices:['View all departments','View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+    }
+]
+
+const addDpQ = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter the name of this department:',
+    }
+]
+
+const addRoleQ = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter the name of this role:',
+    },
+    {
+        type: 'input',
+        name: 'salary',
+        message: 'Enter the salary of this role:',
+    },
+    {
+        type: 'input',
+        name: 'department',
+        message: 'Enter the department of this role:',
+    }
+]
+
+const addEmployeeQ = [
+    {
+        type: 'input',
+        name: 'first_name',
+        message: 'Enter the first name of this employee:',
+    },
+    {
+        type: 'input',
+        name: 'last_name',
+        message: 'Enter the last name of this employee:',
+    },
+    {
+        type: 'input',
+        name: 'role',
+        message: 'Enter the role of this employee:',
+    },
+    {
+        type: 'input',
+        name: 'manager',
+        message: 'Enter the name of manager of employee, (Enter null if this employee has no manager)',
+    }
+]
